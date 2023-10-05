b83835cecf69fce245cdbcf5c83bd39455ea058f
piratemermaid <schnur.ariel@gmail.com>
2023-10-05 14:49:54 -0600

    Fix user blades fetch

--- server/routes/userData.js
+++ server/routes/userData.js
@@ -7,7 +7,11 @@ const router = new Router();
 router.get("/blades", async (req, res, next) => {
     const user = await User.query().findById(1).withGraphFetched("blades");
 
-    res.send(user.blades.map(({ name }) => name));
+    res.send(
+        user.blades.map(({ name }) => {
+            return { name };
+        })
+    );
 });
 
 module.exports = router;
